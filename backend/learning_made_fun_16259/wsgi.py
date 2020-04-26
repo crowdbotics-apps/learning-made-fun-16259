a22eab243c443a9f21971f7c7b2f6a95ca81ee58
Crowdbotics <team@crowdbotics.com>
2020-04-26 03:19:01 +0000

    Update models

--- backend/learning_made_fun_16259/wsgi.py
+++ backend/learning_made_fun_16259/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'learning_made_fun_16259.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learning_made_fun_16259.settings")
 
 application = get_wsgi_application()
